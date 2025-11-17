96526ea34584bce223b5cb64828734b9c0b033aa
Praveen-mano <ppraveenthb@gmail.com>
2025-11-17 17:19:32 +0530

    Delete frontend/constants.js

--- frontend/constants.js
+++
@@ -1,8 +0,0 @@
-export const CompilerPhaseName = {
-  LEXICAL_ANALYSIS: "Lexical Analysis",
-  SYNTAX_ANALYSIS: "Syntax Analysis",
-  SEMANTIC_ANALYSIS: "Semantic Analysis",
-  INTERMEDIATE_CODE_GENERATION: "Intermediate Code Generation",
-  OPTIMIZATION: "Optimization",
-  CODE_GENERATION: "Code Generation",
-};
