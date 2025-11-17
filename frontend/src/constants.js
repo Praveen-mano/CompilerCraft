78d5aab5ee573c362ea7cd032dbd9959b1cc8ee6
Praveen-mano <ppraveenthb@gmail.com>
2025-11-17 17:18:07 +0530

    Add files via upload

---
+++ frontend/src/constants.js
@@ -0,0 +1,8 @@
+export const CompilerPhaseName = {
+  LEXICAL_ANALYSIS: "Lexical Analysis",
+  SYNTAX_ANALYSIS: "Syntax Analysis",
+  SEMANTIC_ANALYSIS: "Semantic Analysis",
+  INTERMEDIATE_CODE_GENERATION: "Intermediate Code Generation",
+  OPTIMIZATION: "Optimization",
+  CODE_GENERATION: "Code Generation",
+};
